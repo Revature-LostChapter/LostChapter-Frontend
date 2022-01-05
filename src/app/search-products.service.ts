f4dde2a8f8821a87729fdd8721e3ebf96e197e1b
Damilare Olaleye <damilare.olaleye@cyatmr.com>
2022-01-05 16:00:09 -0600

    add cors stuffs

--- src/app/search-products.service.ts
+++ src/app/search-products.service.ts
@@ -9,7 +9,7 @@ export class SearchProductsService {
   constructor(private http: HttpClient) { }
 
   getSearchResult(searchItem: string){
-    return this.http.get(`http://localhost:8081/books/search/${searchItem}`, {
+    return this.http.get(`http://ec2-18-219-69-82.us-east-2.compute.amazonaws.com:8081/http://localhost:8081/books/search/${searchItem}`, {
       observe: 'response'
     })
   }
@@ -19,7 +19,7 @@ export class SearchProductsService {
     let parameter = new HttpParams();
     parameter = parameter.append('productId', productId);
     parameter = parameter.append('quantity', quantity);
-    return this.http.post(`http://localhost:8081/users/${userId}/cart`, {},
+    return this.http.post(`http://ec2-18-219-69-82.us-east-2.compute.amazonaws.com:8081/http://localhost:8081/users/${userId}/cart`, {},
       {
         "params": parameter,
         withCredentials: true,
