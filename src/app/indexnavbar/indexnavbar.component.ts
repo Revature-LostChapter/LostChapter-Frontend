bc35867f2d3931c099878f38e812101133a2e06c
Rafael Dantas <rafambd@gmai.com>
2022-01-05 16:26:53 -0500

    Successfully got logged in username to show up, started code to make navbar dynamically change, we'll just need to make it check router

--- src/app/indexnavbar/indexnavbar.component.ts
+++ src/app/indexnavbar/indexnavbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'User';
+import { LoginService } from '../login.service';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-indexnavbar',
@@ -6,13 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./indexnavbar.component.css']
 })
 export class IndexnavbarComponent implements OnInit {
-  loggedInTrue:boolean= true;
-  constructor() { }
+  loggedIn:boolean= false;
+  notLoggedIn:boolean= true;
+  ableToSignUp:boolean= true;
+  ableToLogIn:boolean= true;
+  constructor(private loginService:LoginService) { }
+  
+  getUserStatus(){
+    
+  }
+  
+  currentUser!: String;
+  
+  checkIfLoggedIn() {
+    this.loginService.checkLoginStatus().subscribe((res) => {
+      if (res.status === 200 || res.status === 201){ // depending on the status
+        let body = <User> res.body;
+        this.ableToSignUp = !this.ableToSignUp;
+        this.currentUser = body.username;
+        this.ableToLogIn = !this.ableToLogIn;
+        this.loggedIn = !this.loggedIn;
+        this.notLoggedIn = !this.notLoggedIn; 
+      }
+    },
+    (err) => {
 
-
-
+    });
+  }
 
   ngOnInit(): void {
+    this.checkIfLoggedIn();
     // get current signed in user, so it will be used to toggle loggedInTrue and show the user's username
   }
 }
