80f1bb25e1c906176b9e94f8fa856d315aba2fc9
Rafael Dantas <rafambd@gmai.com>
2022-01-04 23:49:11 -0500

    95% done with footer, just need to troubleshoot something small, there's a note in the html of the component. And started working on Nav task

--- src/app/indexnavbar/indexnavbar.component.ts
+++ src/app/indexnavbar/indexnavbar.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./indexnavbar.component.css']
 })
 export class IndexnavbarComponent implements OnInit {
-
+  loggedInTrue:boolean= true;
   constructor() { }
+
+  userProfile= "account_circle"
 
 
   ngOnInit(): void {
+    // get current signed in user, so it will be used to toggle loggedInTrue and show the user's username
   }
 }
