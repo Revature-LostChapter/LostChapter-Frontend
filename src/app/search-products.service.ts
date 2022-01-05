085be224c8b0b7045808daa5249ce1f6fbbfd2b8
Damilare Olaleye <92756666+damilare-olaleye@users.noreply.github.com>
2022-01-05 15:45:35 -0600

    Update search-products.service.ts

--- src/app/search-products.service.ts
+++ src/app/search-products.service.ts
@@ -9,7 +9,7 @@ export class SearchProductsService {
   constructor(private http: HttpClient) { }
 
   getSearchResult(searchItem: string){
-    return this.http.get(`http://localhost:9090/books/search/${searchItem}`, {
+    return this.http.get(`http://localhost:8081/books/search/${searchItem}`, {
       observe: 'response'
     })
   }
@@ -19,7 +19,7 @@ export class SearchProductsService {
     let parameter = new HttpParams();
     parameter = parameter.append('productId', productId);
     parameter = parameter.append('quantity', quantity);
-    return this.http.post(`http://localhost:9090/users/${userId}/cart`, {},
+    return this.http.post(`http://localhost:8081/users/${userId}/cart`, {},
       {
         "params": parameter,
         withCredentials: true,
