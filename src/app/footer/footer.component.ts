2f5060d097aa609de1c6b8ca7a9fc4b67f03df2d
Damilare Olaleye <damilare.olaleye@cyatmr.com>
2022-01-05 20:14:37 -0600

    add dark mode and its configuration

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {faLightbulb as faSolidLightbulb, faDollarSign, IconDefinition
+} from "@fortawesome/free-solid-svg-icons";
+import { faLightbulb as faRegularLightbulb } from "@fortawesome/free-regular-svg-icons";
+import { ThemeService } from "src/app/theme.service";
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  faLightbulb!: IconDefinition;
+
+  constructor(private themeService: ThemeService) { }
 
   ngOnInit(): void {
+    this.setLightbulb();
+  }
+
+  setLightbulb() {
+    if (this.themeService.isDarkTheme()) {
+      this.faLightbulb = faRegularLightbulb;
+    } else {
+      this.faLightbulb = faSolidLightbulb;
+    }
+  }
+
+  toggleTheme() {
+    if (this.themeService.isDarkTheme()) {
+      this.themeService.setLightTheme();
+    } else {
+      this.themeService.setDarkTheme();
+    }
+
+    this.setLightbulb();
   }
 
 }
---
+++ src/app/theme.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { dark, light, Theme } from './theme/theme.module';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+
+  constructor() { }
+
+  private active: Theme = light;
+  private availableThemes: Theme[] = [light, dark];
+
+  getAvailableThemes(): Theme[] {
+    return this.availableThemes;
+  }
+
+  getActiveTheme(): Theme {
+    return this.active;
+  }
+
+  isDarkTheme(): boolean {
+    return this.active.name === dark.name;
+  }
+
+  setDarkTheme(): void {
+    this.setActiveTheme(dark);
+  }
+
+  setLightTheme(): void {
+    this.setActiveTheme(light);
+  }
+
+  setActiveTheme(theme: Theme): void {
+    this.active = theme;
+
+    Object.keys(this.active.properties).forEach(property => {
+      document.documentElement.style.setProperty(
+        property,
+        this.active.properties[property]
+      );
+    });
+  }
+
+}
