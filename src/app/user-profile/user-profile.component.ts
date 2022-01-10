66cb037ea90e4dcdbf21186a9528e746328bffc9
ramhors <rjm2075@gmail.com>
2022-01-10 16:10:30 -0500

    display user profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'User';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
+  /*currentUser: User = {
+    /*  id: 0,
+    username: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    age: 0,
+    email: '',
+    birthday: '',
+    address: '',
+    role: '', 
+  }; */
+  currentUser!: User;
 
-  constructor() { }
+  async getLoggedUser() {
+    this.loginService.checkLoginStatus().subscribe((res) => {
+      console.log(res);
+      if (res.status === 200) {
+        let body = <User>res.body;
+        this.currentUser = body;
+        console.log(this.currentUser);
+      }
+    });
+    console.log();
+  }
+  public settingUser(newUser: User): void {}
+
+  constructor(private router: Router, private loginService: LoginService) {}
 
   ngOnInit(): void {
+    this.getLoggedUser();
   }
-
 }
