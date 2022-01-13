2141c2746736b65a9f6d29fdb8477512deef77c1
ramhors <rjm2075@gmail.com>
2022-01-13 11:27:30 -0500

    git update user-profile

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -2,36 +2,44 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   checkLoginStatus() {
     return this.http.get('http://localhost:8081/loginstatus', {
       observe: 'response',
-      withCredentials: true
-    })
+      withCredentials: true,
+    });
   }
 
-  login(username: string, password: string){
-    return this.http.post('http://localhost:8081/login', {
-      "access": username,
-      "password": password
-    }, {
-      withCredentials: true,
-      observe: 'response'
-    })
+  login(username: string, password: string) {
+    return this.http.post(
+      'http://localhost:8081/login',
+      {
+        access: username,
+        password: password,
+      },
+      {
+        withCredentials: true,
+        observe: 'response',
+      }
+    );
   }
 
   logout() {
-    return this.http.post('http://localhost:8081/logout', {},{
-      observe: 'response',
-      withCredentials: true,
-      responseType: 'text'
-    })
+    return this.http.post(
+      'http://localhost:8081/logout',
+      {},
+      {
+        observe: 'response',
+        withCredentials: true,
+        responseType: 'text',
+      }
+    );
   }
-
-
+  updateUser() {
+    return this.http.put('http://localhost:8081/update', {});
+  }
 }
