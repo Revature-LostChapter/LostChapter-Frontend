75f155c61cfa45bbe67a01ee947e149e37cf99a8
leon <leonhakimi@gmail.com>
2022-01-14 15:16:09 -0800

    change update user endpoint

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -66,7 +66,6 @@ export class LoginService {
       {
         withCredentials: true,
         observe: 'response',
-        responseType: 'text',
       }
     );
   }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,61 +7,61 @@ import { LoginService } from '../login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor(private router: Router, private loginService: LoginService) { }
+  constructor(private router: Router, private loginService: LoginService) {}
 
   ngOnInit(): void {
     this.checkIfLoggedIn();
-
   }
 
   username!: string;
   password!: string;
   errorMessage!: string;
 
-
   // show / hide password
   hide = true;
-  
+
   // perform service layer functionality here
   onLoggedIn() {
-    this.loginService.login(this.username, this.password).subscribe((res) => {
-      if (res.status === 201 || res.status === 200) {
-        let body = <User> res.body;
-        console.log(res);
-        if (body.role === 'Customer'){
-          this.router.navigate(['/home']); // navigates to customer route page -> redirecting to this route for now until we have full functionalities of the routes
-        }
+    this.loginService.login(this.username, this.password).subscribe(
+      (res) => {
+        if (res.status === 201 || res.status === 200) {
+          let body = <User>res.body;
+          console.log(res);
+          if (body.role === 'Customer') {
+            this.router.navigate(['/home']); // navigates to customer route page -> redirecting to this route for now until we have full functionalities of the routes
+          }
 
-        if (body.role === 'Admin'){
-          this.router.navigate(['admin']); // navigates to admin route page
+          if (body.role === 'Admin') {
+            this.router.navigate(['admin']); // navigates to admin route page
+          }
         }
+      },
+      (err) => {
+        this.errorMessage = err.error;
       }
-    }, (err) => {
-      this.errorMessage = err.error;
-    });
+    );
   }
 
   checkIfLoggedIn() {
-    this.loginService.checkLoginStatus().subscribe((res) => {
-      if (res.status === 200 || res.status === 201){ // depending on the status
-        let body = <User> res.body;
+    this.loginService.checkLoginStatus().subscribe(
+      (res) => {
+        if (res.status === 200 || res.status === 201) {
+          // depending on the status
+          let body = <User>res.body;
 
-        if(body.role === 'Customer'){
-          this.router.navigate(['']);
+          if (body.role === 'Customer') {
+            this.router.navigate(['']);
+          }
+
+          if (body.role === 'Admin') {
+            this.router.navigate(['/admin']);
+          }
         }
-
-        if(body.role === 'Admin'){
-          this.router.navigate(['/admin']);
-        }
-      }
-    },
-    (err) => {
-
-    });
+      },
+      (err) => {}
+    );
   }
-
 }
